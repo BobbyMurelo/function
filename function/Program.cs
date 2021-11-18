3e31603889950969c65dbcfa21afc4a36d4c83aa
bobby murelo <murelobobby99@gmail.com>
2021-11-18 16:39:16 +0200

    Add project files.

---
+++ function/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace function
+{                        //2016102@vumk.eu
+                         //Student name:Bobina Murelo. 
+    class Program
+    {
+   
+            static void Main(string[] args)
+            {
+                int counter, number, fact;
+
+                Console.WriteLine("Please enter the number you wish to factorize");
+                number = int.Parse(Console.ReadLine());
+                fact = number;
+
+                for (counter = number - 1; counter >= 1; counter--)
+                {
+                    fact = fact * counter;
+                }
+                Console.WriteLine("The number you entered was {0} and it's factorial is {1}", number, fact);
+                Console.ReadLine();
+            }
+        }
+    }
+
+    
+
